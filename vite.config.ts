c0df41437a7a32796173b83c01a0580a11c7db9c
imran <imran8122005@gmail.com>
2025-07-07 21:28:13 +0530

    deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/cosmic-chess-coder-imran/' : '/',
   server: {
     host: "::",
     port: 8080,
